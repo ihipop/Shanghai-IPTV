ca7639ed92ab6cefb9d83c44ff4324d76634dc01
ihipop <ihipop@gmail.com>
2021-07-28 14:32:55 +0800

    完善server mode

--- server.php
+++ server.php
@@ -10,16 +10,21 @@ parse_str($argv[1], $args);
 $udpxy    = $args['udpxy'] ?? null;
 $fileName = $args['file'] ?? 'tel_mu';
 $fileName = basename($fileName) . '.m3u8';
+$m3u8     = null;
 if (!file_exists($fileName)) {
     $m3u8 = include './export.php';
-    file_put_contents($fileName, $m3u8);
+    if (empty($udpxy)) {
+        file_put_contents($fileName, $m3u8);
+    }
 }
 if (empty($udpxy)) {
     header('Location: ' . $fileName);
     exit();
 }
-$m3u8 = file_get_contents($fileName);
+if (empty($m3u8)) {
+    $m3u8 = file_get_contents($fileName);
+}
 
-header('Content-type:application/x-mpegURL; charset=utf-8');
+header('Content-type:application/vnd.apple.mpegurl; charset=utf-8');
 header('Content-Disposition: attachment; filename="' . $fileName . '"');
 echo str_replace('rtp://', rtrim($udpxy, ' \t\n\r\0\x0B/') . '/rtp/', $m3u8);
