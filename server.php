c202cd2101d70148e88682b6764adbf65883e51d
ihipop <ihipop@gmail.com>
2026-01-04 10:44:54 +0800

    移除服务器模式的文件缓存

--- server.php
+++ server.php
@@ -7,24 +7,13 @@ define('SERVER_MODE', true);
 $argv[1] = $_SERVER['QUERY_STRING'] ?? '';
 $args    = [];
 parse_str($argv[1], $args);
-$udpxy    = $args['udpxy'] ?? null;
 $fileName = $args['file'] ?? 'tel_mu';
 $fileName = basename($fileName) . '.m3u8';
-$m3u8     = null;
-if (!file_exists($fileName)) {
-    $m3u8 = include './export.php';
-    if (empty($udpxy)) {
-        file_put_contents($fileName, $m3u8);
-    }
-}
-if (empty($udpxy)) {
-    header('Location: ' . $fileName);
-    exit();
-}
-if (empty($m3u8)) {
-    $m3u8 = file_get_contents($fileName);
-}
 
-header('Content-type:application/vnd.apple.mpegurl; charset=utf-8');
-header('Content-Disposition: attachment; filename="' . $fileName . '"');
-echo str_replace('rtp://', rtrim($udpxy, ' \t\n\r') . '/rtp/', $m3u8);
+// 每次都从数据库导出,确保数据是最新的
+$m3u8 = include './export.php';
+
+
+header('Content-Type: application/vnd.apple.mpegurl; charset=utf-8');
+header('Content-Disposition: attachment; filename*=UTF-8\'\'' . rawurlencode($fileName));
+echo $m3u8;
